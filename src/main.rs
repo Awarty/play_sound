59c8b640d96ac8fcd7c9f100aafdf6b2767b1a9e
Jacob Almrot <jacob.almrot@gmail.com>
2023-05-30 15:23:32 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ fn main() {
     let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
     let sink = rodio::Sink::try_new(&handle).unwrap();
 
-    //let file = std::fs::File::open("/home/root/StarWars3.wav").unwrap();
-    //sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+    let file = std::fs::File::open("/home/root/StarWars3.wav").unwrap();
+    sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
 
-    //sink.sleep_until_end();
+    sink.sleep_until_end();
 }
